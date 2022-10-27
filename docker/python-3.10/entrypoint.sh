a00b7540f34e1fee8aa69889e40cbfd80beee7ee
Raphael Castro <rafaphcastro@gmail.com>
2022-10-27 01:03:00 -0300

    fix: auth configuration

---
+++ docker/python-3.10/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="$PATH:$HOME/.local/bin"
+
+exec "$@"
--- docker/python-3.11/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH="$PATH:/home/notte/.local/bin"
-
-if [ -f "requirements.txt" ]; then
-  pip install -r requirements.txt
-fi
-
-exec "$@"
